de27cee93f1b27af8b42e928069dffe6de84a793
sreerenjini-keyvalue <sreerenjini@keyvalue.systems>
2023-09-15 12:31:15 +0530

    Add Style Editor

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,10 +3,24 @@ import { ReactComponent as ErrorIcon } from './ErrorIcon.svg';
 import { ReactComponent as ErrorCloseIcon } from './CloseError.svg';
 import { ReactComponent as TickCircleIcon } from './TickCircle.svg';
 import { ReactComponent as CloseIcon } from './Close.svg';
+import { ReactComponent as ChevronRight } from './ChevronRight.svg';
+import { ReactComponent as ChevronLeft } from './ChevronLeft.svg';
 
 import { ReactComponent as Category } from './Category.svg';
 import { ReactComponent as HomeIcon } from './HomeIcon.svg';
 import { ReactComponent as Explore } from './Explore.svg';
 import { ReactComponent as Profile } from './Profile.svg';
 
-export { ErrorIcon, ErrorCloseIcon, TickCircleIcon, CloseButton, CloseIcon, Category, HomeIcon, Explore, Profile };
+export {
+  ErrorIcon,
+  ErrorCloseIcon,
+  TickCircleIcon,
+  CloseButton,
+  CloseIcon,
+  ChevronRight,
+  ChevronLeft,
+  Category,
+  HomeIcon,
+  Explore,
+  Profile
+};
