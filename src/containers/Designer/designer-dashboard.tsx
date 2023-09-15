db4a41d531646ad5fddc1e8988a91f7d1a50e945
treesa.j <treesa.j@keyvalue.systems>
2023-09-15 14:06:19 +0530

    navbar

--- src/containers/Designer/designer-dashboard.tsx
+++ src/containers/Designer/designer-dashboard.tsx
@@ -1,15 +1,55 @@
-import { Box } from '@mui/material';
-import styles from './styles';
-import './styles.css';
-import { DesignerContextProvider } from './designer-context';
-import { Navbar } from './navbar';
+import { Box } from "@mui/material";
+import styles from "./styles";
+import "./styles.css";
+import { DesignerContextProvider } from "./designer-context";
+import { Navbar } from "./navbar";
+import {
+  DesignsIcon,
+  HomeIcon,
+  MagicIcon,
+  MaterialsIcon,
+  OrdersIcon,
+} from "@assets/icons";
+import { useState } from "react";
 
 const DesignerDashboard = () => {
+  const navbarItems = [
+    {
+      id: 0,
+      title: "Home",
+      icon: <HomeIcon />,
+    },
+    {
+      id: 1,
+      title: "Designs",
+      icon: <DesignsIcon />,
+    },
+    {
+      id: 2,
+      title: "Materials",
+      icon: <MaterialsIcon />,
+    },
+    {
+      id: 3,
+      title: "Orders",
+      icon: <OrdersIcon />,
+    },
+    {
+      id: 4,
+      title: "AI Magic",
+      icon: <MagicIcon />,
+    },
+  ];
+
+  const [activeTab, setActiveTab] = useState(0);
+
   return (
     <DesignerContextProvider>
       <Box sx={[styles.root]}>
-        <div className='h-full flex flex-col space-y-5 p-[48px] bg-green-1'>Hey there</div>
-        <Navbar />
+        <div className="h-full flex flex-col space-y-5 p-[48px] bg-white">
+          {navbarItems.find((item) => item.id === activeTab)?.title}
+        </div>
+        <Navbar navbarItems={navbarItems} setActiveTab={setActiveTab} />
       </Box>
     </DesignerContextProvider>
   );
